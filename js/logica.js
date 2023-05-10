47971f05c6a0e2645e10443bb76067fded1fb6c8
Gabriel Ivan Carrillo <gabrielcarrillo943@gmail.com>
2023-05-10 16:11:55 -0300

    first responsive

--- js/logica.js
+++ js/logica.js
@@ -2,12 +2,14 @@ let bill;
 let people;
 let tip;
 let total;
+let custom;
 const Percent = {
     five: 5 / 100,
     ten: 10  / 100,
     fifteen: 15  / 100,
     twentyfive: 25  / 100,
-    fifty: 50  / 100
+    fifty: 50  / 100,
+    custom: 1 /100
 }
 
 // functions
@@ -17,12 +19,17 @@ function getBill(){
 }
 
 function getPeople(){
-    people = parseFloat(document.getElementById("people").value);
+    people = parseInt(document.getElementById("people").value);
     return people;
 }
 
 function perPerson(){
     return bill / people;
+}
+
+function getCustom(){
+    custom = parseInt(document.getElementById("custom").value);
+    Percent.custom = custom / 100;
 }
 
 function fivePercentTip(){
@@ -55,6 +62,12 @@ function fiftyPercentTip(){
     document.getElementById("total").value = "$"+(perPerson()+tip).toFixed(2);
 }
 
+function customTip(){
+    tip = perPerson() * Percent.custom;
+    document.getElementById("tipAmount").value = "$"+tip.toFixed(2);
+    document.getElementById("total").value = "$"+(perPerson()+tip).toFixed(2);
+}
+
 function warnPeople(){
     if (people == isNaN || people == 0){
         document.getElementById("warning").innerHTML = "Can't be zero!"
@@ -65,9 +78,11 @@ function warnPeople(){
 
 bill = document.getElementById("bill").addEventListener("change", getBill);
 people = document.getElementById("people").addEventListener("change", getPeople);
+custom = document.getElementById("custom").addEventListener("change", getCustom);
 let fiver = document.getElementById("fivper").addEventListener("click", fivePercentTip);
 let tenner = document.getElementById("tenper").addEventListener("click", tenPercentTip)
 let fifteener = document.getElementById("fifper").addEventListener("click", fifteenPercentTip)
 let quarter = document.getElementById("tweper").addEventListener("click", twentyFivePercentTip)
 let half = document.getElementById("halper").addEventListener("click", fiftyPercentTip)
+let customize = document.getElementById("custom").addEventListener("blur", customTip)
 document.getElementById("people").addEventListener("blur", warnPeople)
