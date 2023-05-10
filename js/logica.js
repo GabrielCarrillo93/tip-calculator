512ee64e68cf4433d494f4ffca65bea5da7938aa
Gabriel Ivan Carrillo <gabrielcarrillo943@gmail.com>
2023-05-10 14:31:01 -0300

    warning added

--- js/logica.js
+++ js/logica.js
@@ -1,18 +1,16 @@
-let bill = "";
-let people = "";
-let tip = bill / people;
-let total = "";
+let bill;
+let people;
+let tip;
+let total;
+const Percent = {
+    five: 5 / 100,
+    ten: 10  / 100,
+    fifteen: 15  / 100,
+    twentyfive: 25  / 100,
+    fifty: 50  / 100
+}
 
-let fiver = document.getElementById("fivper").addEventListener("click", fivePercentTip);
-let tenner = document.getElementById("tenper").addEventListener("click", tenPercentTip)
-let fifteener = document.getElementById("fifper").addEventListener("click", fifteenPercentTip)
-let quarter = document.getElementById("tweper").addEventListener("click", twentyFivePercentTip)
-let half = document.getElementById("halper").addEventListener("click", fiftyPercentTip)
-
-
-bill = document.getElementById("bill").addEventListener("change", getBill);
-people = document.getElementById("people").addEventListener("change", getPeople);
-
+// functions
 function getBill(){
     bill = parseFloat(document.getElementById("bill").value);
     return bill;
@@ -28,31 +26,48 @@ function perPerson(){
 }
 
 function fivePercentTip(){
-    tip = perPerson() * 5 / 100;
+    tip = perPerson() * Percent.five;
     document.getElementById("tipAmount").value = "$"+tip.toFixed(2);
     document.getElementById("total").value = "$"+(tip + perPerson()).toFixed(2);
 }
 
 function tenPercentTip(){
-    tip = perPerson() * 10 / 100
+    tip = perPerson() * Percent.ten;
     document.getElementById("tipAmount").value = "$"+tip.toFixed(2);
     document.getElementById("total").value = "$"+(tip + perPerson()).toFixed(2);
 }
 
 function fifteenPercentTip(){
-    tip = perPerson() * 15 / 100;
+    tip = perPerson() * Percent.fifteen;
     document.getElementById("tipAmount").value = "$"+tip.toFixed(2);
     document.getElementById("total").value = "$"+(perPerson()+tip).toFixed(2);
 }
 
 function twentyFivePercentTip(){
-    tip = perPerson() * 25 / 100;
+    tip = perPerson() * Percent.twentyfive;
     document.getElementById("tipAmount").value = "$"+tip.toFixed(2);
     document.getElementById("total").value = "$"+(perPerson()+tip).toFixed(2);
 }
 
 function fiftyPercentTip(){
-    tip = perPerson() * 50 / 100;
+    tip = perPerson() * Percent.fifty;
     document.getElementById("tipAmount").value = "$"+tip.toFixed(2);
     document.getElementById("total").value = "$"+(perPerson()+tip).toFixed(2);
 }
+
+function warnPeople(){
+    if (people == isNaN || people == 0){
+        document.getElementById("warning").innerHTML = "Can't be zero!"
+    } else if (people >= 1){
+        document.getElementById("warning").innerHTML = " "
+    }
+}
+
+bill = document.getElementById("bill").addEventListener("change", getBill);
+people = document.getElementById("people").addEventListener("change", getPeople);
+let fiver = document.getElementById("fivper").addEventListener("click", fivePercentTip);
+let tenner = document.getElementById("tenper").addEventListener("click", tenPercentTip)
+let fifteener = document.getElementById("fifper").addEventListener("click", fifteenPercentTip)
+let quarter = document.getElementById("tweper").addEventListener("click", twentyFivePercentTip)
+let half = document.getElementById("halper").addEventListener("click", fiftyPercentTip)
+document.getElementById("people").addEventListener("blur", warnPeople)
